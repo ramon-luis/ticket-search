47472da72ef1f804d4b018ad1676efd402099af2
RAM0N_LUIS <ramonlrodriguez@uchicago.edu>
2016-07-07 20:22:18 -0500

    trying to process multiple records

--- src/main/java/seatGeekPortal/CaptureWebDriver.java
+++ src/main/java/seatGeekPortal/CaptureWebDriver.java
@@ -11,49 +11,111 @@ import java.util.concurrent.*;
 public class CaptureWebDriver {
 
     public static void main(String[] args) {
+
+        String sTestQuery = "Chicago Cubs";
+        String sQuery = sTestQuery.replace(" ", "+");
+
         ExecutorService exService = Executors.newSingleThreadExecutor();
-        FutureTask<ArrayList<Event>> futureTask= new FutureTask<ArrayList<Event>>(new GetEventsCallable());
+        FutureTask<ArrayList<Event>> futureTask= new FutureTask<ArrayList<Event>>(new GetEventsCallable(sQuery));
         exService.execute(futureTask);
 //        //checks if task done
 //        System.out.println(futureTask.isDone());
 //        //checks if task canceled
 //        System.out.println(futureTask.isCancelled());
-        //fetches result and waits if not ready
-        try {
-            ArrayList<Event> results = futureTask.get();
-            for (Event result : results) {
-                System.out.println(result.getTitle() + " : " + result.getVenue().getName());
+//        // fetches result and waits if not ready
+
+
+            try {
+                ArrayList<Event> results = futureTask.get();
+                for (Event result : results) {
+                    System.out.println(result.getTitle() + " : " + result.getVenue().getName());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+
+            futureTask.cancel(true);
 
 
     }
 }
 
 class GetEventsCallable implements Callable<ArrayList<Event>> {
-    private static final String HTTP_SERVICES_SEAT_GEEK_CHICAGO_CUBS = "https://api.seatgeek.com/2/events?q=chicago+cubs&sort=score.desc&per_page=10";
+
+    // Strings for HTTP query of SeatGeek API
+    private static final String SEAT_GEEK_API_QUERY = "https://api.seatgeek.com/2/events?q=";
+    private static final String RESULTS_PER_PAGE_TAG = "&per_page=";
+    private static final String PAGE_TAG = "&page=20";
+    private static final String SORT_TAG = "&sort=score.desc";
+    private static final String CLIENT_ID_TAG = "&client_id=NTEyODcwNnwxNDY3NzUzOTYy";
+
+    private String sQuery;
+
+    public GetEventsCallable(String sQuery) {
+        this.sQuery = sQuery;
+    }
+
+
     public ArrayList<Event> call() {
 
-        String str = null;
+
+        int iPage = 1;
+        int iTotalRecordsAvailable = -1;
+        int iRecordsProcessed = 0;
+        int iRecordsPerPage = 10;
+        String sData = null;
+        EventData eventData = null;
+        ArrayList<Event> events = new ArrayList<>();
+
+        // get number of records available
         try {
-            str = JsonParser.sendGet(HTTP_SERVICES_SEAT_GEEK_CHICAGO_CUBS);
+            sData = JsonParser.sendGet(SEAT_GEEK_API_QUERY + sQuery + CLIENT_ID_TAG);
+            eventData = new Gson().fromJson(sData, EventData.class);
+            iTotalRecordsAvailable = eventData.getMeta().getTotal();
+            System.out.println("Total records avail: " + iTotalRecordsAvailable);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (str == null){
-            System.out.println("Sorry, I couldn't find your data");
+
+        // increment page number and add events to list
+        while (iRecordsProcessed < iTotalRecordsAvailable) {
+            try {
+                System.out.println("trying to add data from page: " + iPage);
+                sData = JsonParser.sendGet(SEAT_GEEK_API_QUERY + sQuery + SORT_TAG + PAGE_TAG + iPage + CLIENT_ID_TAG);
+                eventData = new Gson().fromJson(sData, EventData.class);
+                events.addAll(eventData.getEvents());
+                iRecordsProcessed = events.size();
+                System.out.println("Records processed: " + iRecordsProcessed);
+                iPage++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
-        //Event eventData = new Gson().fromJson(str, Event.class);
-        EventData eventData = new Gson().fromJson(str, EventData.class);
-        //EventData eventData = eventData.getEvents();
-        ArrayList<Event> eventsList = new ArrayList<>(eventData.getEvents());
+        System.out.println("Exited Loop");
+        return events;
 
 
-        return eventsList;
+//
+//        String str = null;
+//        try {
+//            str = JsonParser.sendGet(SEAT_GEEK_API_QUERY + sQuery + SORT_TAG + PAGE_TAG + CLIENT_ID_TAG);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if (str == null){
+//            System.out.println("Sorry, I couldn't find your data");
+//        }
+//
+//        //Event eventData = new Gson().fromJson(str, Event.class);
+//        EventData eventData = new Gson().fromJson(str, EventData.class);
+//        System.out.println(eventData.getMeta().getTotal());
+//        //EventData eventData = eventData.getEvents();
+//        ArrayList<Event> eventsList = new ArrayList<>(eventData.getEvents());
+//
+//
+//        return eventsList;
     }
 }
